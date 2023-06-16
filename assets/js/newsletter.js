53aba0db2dbdbae483ed14c28f038cb6f9d951d9
Brian Ressler <carlofabyss@hotmail.com>
2023-06-16 14:29:52 -0500

    Print list labels

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -6,11 +6,22 @@ function newsletterLoad() {
 	observer = new MutationObserver(function(mutationsList, observer) {
 		console.log(mutationsList);
 		let tops_emailList_label = document.getElementById("list_memberships_label_0");
+		let tops_emailList_listsLabel = document.getElementsByClassName("ctct-form-checkbox-label");
 		
 		if(tops_emailList_label != null) {
 			tops_emailList_label_inner = tops_emailList_label.innerHTML;
 			tops_emailList_label.innerHTML = "Sign Up!";
 		}
+		
+		if(tops_emailList_listsLabel != null) {
+			if(tops_emailList_listsLabel.length > 0) {
+				for(i = 0; i < tops_emailList_listsLabel.length) {
+					let currentlistlabel = tops_emailList_listsLabel[i];
+					console.log(currentlistlabel.innerHTML);
+				}
+			}
+		}
+		
 	});
 
 	// call 'observe' on that MutationObserver instance, 
